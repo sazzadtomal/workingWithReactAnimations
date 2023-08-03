f590ade884a8a7df5f42f8931755640ff2330afc
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2023-08-03 12:59:26 +0600

    react transition package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import {Transition} from "react-transition-group";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,7 +7,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -22,6 +23,19 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+       
+       
+       <button
+          className="Button"
+          onClick={() =>
+            this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
+        >
+         Yo
+        </button>
+       <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+                {state=>(<div style={{backgroundColor:"red",width:100,height:100, margin:"auto",transition:"all 1s ease-out",opacity: state==="exiting" ? 0 : 1}}></div>)}
+       </Transition>
+
         <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>Open Modal</button>
